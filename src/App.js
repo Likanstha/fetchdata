dc25ae8167c2ca2089d731a01a475486c565131c
likanstha <likan.stha@gmail.com>
2022-07-01 23:29:44 +1000

    delete and add row done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import {Dashboard}  from "./pages/Dashboard.page";
+import { Dashboard } from "./pages/Dashboard.page";
 
 function App() {
   return (
--- src/pages/Dashboard.page.js
+++ src/pages/Dashboard.page.js
@@ -1,47 +1,62 @@
-import React,{ useState } from 'react'
-import { Container,Row,Col,Button } from 'react-bootstrap'
-import {UniTable} from "../components/UniTable";
+import React, { useState } from 'react'
+import { Container, Row, Col, Button } from 'react-bootstrap'
+import { UniTable } from "../components/UniTable";
 //import lists from '../utils/dummyfile.json'
 
 export const Dashboard = () => {
 
-const [lists, setlists] = useState(null);
+    const [lists, setlists] = useState(null);
 
-const fetchData = () => {
-    fetch('http://universities.hipolabs.com/search?country=Australia')
-    .then(response => response.json())
-    .then(data => setlists(data));
-};
+    const fetchData = () => {
+        fetch('http://universities.hipolabs.com/search?country=Australia')
+            .then(response => response.json())
+            .then(data => setlists(data));
+    };
 
-  return (
-    <Container>
+    const handleDelete = () => {
+        const newLists = [...lists];
+        newLists.pop();
+        // console.log('newLists', newLists);
+        setlists(newLists);
+    };
+
+    const handleAdd = () => {
+        const newLists = [...lists];
+        let newItem = newLists[0];
+        setlists([...newLists, newItem])
+        //console.log('newLists', newLists);
+    }
+    return (
+        <Container>
             <Row>
                 <Col className="text-center">
-                   
-                    <Button  
-                    style={{fontSize:"2rem",padding:"10px 30px"}}
-                    onClick={fetchData}            
+
+                    <Button
+                        style={{ fontSize: "2rem", padding: "10px 30px" }}
+                        onClick={fetchData}
                     >
-                    Load
+                        Load
                     </Button>
 
-                    <Button 
-                    style={{fontSize:"2rem",padding:"10px 30px"}}
+                    <Button
+                        style={{ fontSize: "2rem", padding: "10px 30px" }}
+                        onClick={handleDelete}
                     >
-                    Delete
+                        Delete
                     </Button>
 
-                    <Button 
-                    style={{fontSize:"2rem",padding:"10px 30px"}}
+                    <Button
+                        style={{ fontSize: "2rem", padding: "10px 30px" }}
+                        onClick={handleAdd}
                     >
-                    Add Row
+                        Add Row
                     </Button>
                 </Col>
-            </Row>                      
-            <hr/>
+            </Row>
+            <hr />
             <Row>
-            {lists && <UniTable lists = {lists} />}
+                {lists && <UniTable lists={lists} />}
             </Row>
         </Container>
-  )
+    )
 }
